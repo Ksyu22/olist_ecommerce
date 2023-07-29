5fc872d0ce63f0fd8ae9439ec410a7633db9f782
Oksana Mishchenko <ksyu.ha@hotmail.com>
2023-07-29 21:45:10 +0200

    transforming csv to an api

---
+++ olist_ecommerce/api/fast.py
@@ -0,0 +1,28 @@
+from  fastapi import FastAPI
+import pandas as pd
+import requests
+
+from scripts.parameters import LOCAL_DATA_PATH
+
+
+app = FastAPI()
+
+@app.get("/customers")
+def build_customers():
+    df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_customers_dataset.csv').to_dict()
+    return df
+
+@app.get("/geolocation")
+def build_geolocation():
+    df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_geolocation_dataset.csv').to_dict()
+    return df
+
+@app.get("/sellers")
+def build_geolocation():
+    df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_sellers_dataset.csv').to_dict()
+    return df
+
+@app.get("/orders")
+def build_geolocation():
+    df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_orders_dataset.csv').to_dict()
+    return df
