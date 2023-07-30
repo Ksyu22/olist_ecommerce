74393ffc0f181f8f1f1600f758909a44b618b1e9
Oksana Mishchenko <ksyu.ha@hotmail.com>
2023-07-30 10:25:17 +0200

    deleting comments

--- olist_ecommerce/api/fast.py
+++ olist_ecommerce/api/fast.py
@@ -12,23 +12,3 @@ app = FastAPI()
 def build_dataset(name):
     df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_{name}_dataset.csv').fillna('').to_dict()
     return df
-
-# @app.get("/customers")
-# def build_customers():
-#     df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_customers_dataset.csv').fillna('').to_dict()
-#     return df
-
-# @app.get("/geolocation")
-# def build_geolocation():
-#     df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_geolocation_dataset.csv').fillna('').to_dict()
-#     return df
-
-# @app.get("/sellers")
-# def build_geolocation():
-#     df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_sellers_dataset.csv').fillna('').to_dict()
-#     return df
-
-# @app.get("/orders")
-# def build_geolocation():
-#     df = pd.read_csv(f'{LOCAL_DATA_PATH}/olist_orders_dataset.csv').fillna('').to_dict()
-#     return df
