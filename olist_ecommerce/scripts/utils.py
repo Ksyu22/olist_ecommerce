fe4a56a0d3ffcadb7156bf24413c1cca47a312d1
Oksana Mishchenko <ksyu.ha@hotmail.com>
2023-08-01 07:54:44 +0200

    haversine distance function

---
+++ olist_ecommerce/scripts/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def haversine_distance(df,
+                       start_lat="start_lat",
+                       start_lon="start_lon",
+                       end_lat="end_lat",
+                       end_lon="end_lon"):
+    """
+    Compute distance in km between two pairs of coordinates (lon, lat1)
+    """
+
+    lat_1_rad, lon_1_rad = np.radians(df[start_lat].astype(float)), np.radians(df[start_lon].astype(float))
+    lat_2_rad, lon_2_rad = np.radians(df[end_lat].astype(float)), np.radians(df[end_lon].astype(float))
+    dlon = lon_2_rad - lon_1_rad
+    dlat = lat_2_rad - lat_1_rad
+
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat_1_rad) * np.cos(lat_2_rad) * np.sin(dlon / 2.0) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    haversine_distance = 6371 * c
+    return haversine_distance
